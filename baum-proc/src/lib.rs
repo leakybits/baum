fbb7723d3540703c9b25090da328006d64da6429
James Houlahan <james@houlahan.ch>
2024-02-18 00:38:03 +0100

    docs: Add baum-proc docstrings

--- baum-proc/src/lib.rs
+++ baum-proc/src/lib.rs
@@ -3,13 +3,14 @@
 #[macro_use]
 extern crate proc_util;
 
+/// Implements the `idx!` macro.
 mod idx;
-mod macros;
 
-use proc_macro::TokenStream;
+/// Implements crate-local helper macros.
+mod macros;
 
 /// Converts a (possibly nested) tuple of expressions into an (equally nested) tuple of unique identifiers.
 #[proc_macro]
-pub fn idx(args: TokenStream) -> TokenStream {
+pub fn idx(args: proc_macro::TokenStream) -> proc_macro::TokenStream {
     render!(args, { idx::expand(args) })
 }
